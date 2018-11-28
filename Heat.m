b8fb49e5d7a3f468292c3b62456492f2dcf15b3b
Shawnchik <39521906+Shawnchik@users.noreply.github.com>
2018-11-28 09:46:55 +0900

    Create Heat.m

---
+++ Heat.m
@@ -0,0 +1,11 @@
+function Heat
+% Heat and temperature calculation
+
+% Heat and temperature calculation of ACC side
+HeatAAC
+
+% Heat and temperature calculation of RR side
+HeatRR
+
+% Pass temperature in  next step 
+PassTemperature
