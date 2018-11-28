5276e10772e67f82f310b874550e05a80f1cf885
Shawnchik <39521906+Shawnchik@users.noreply.github.com>
2018-11-28 09:46:33 +0900

    Create CtrlRR.m

---
+++ CtrlRR.m
@@ -0,0 +1,9 @@
+function CtrlRR
+% Control heat pump (RR-1)
+
+global G_Ld Nm_RR
+global INV_FRR Sp_ToutCnRR Sp_ToutCnRR0 Mv_Tout_CnRR0 Sig_INVFRR Flg_PIDINVFRR Kp_INVFRR Ti_INVFRR Mv_Tout_CnRR
+global SpLL_TinCnRR SpUL_TinCnRR
+
+Nm_RR = 1;
+
